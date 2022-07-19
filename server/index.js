bd1a39e0a7c243ac49eefb5b944eced9126a7b56
Micah Norrington <manorrington@gmail.com>
2022-07-18 21:43:54 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+
+// DB connection
+const db = mysql.createConnection({
+  host: "database-1.ci4qghad4q40.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "password",
+  database: "testing",
+  port: 3306,
+});
+
+db.connect((error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("database connection success");
+});
+
+// displaying the data from DB
+app.get("/", function (req, res) {
+  const sqlInsert = "SELECT * FROM testing_table";
+  db.query(sqlInsert, (err, result) => {
+    if (err) {
+      throw error;
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+// port
+app.listen(3001, () => {
+  console.log("listening on 3001");
+});
