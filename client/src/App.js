2f43907f2d6e5765fedd6efbf00244b28f286655
Micah Norrington <manorrington@gmail.com>
2022-07-23 13:03:01 -0400

    backend changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/products").then((res) => {
+      setProductList(res.data);
+    });
+  });
+
   return (
     <div className="App">
-      <p>yo</p>
+      <p>hey</p>
+      {productList.map((val) => {
+        return (
+          <div>
+            <h3>Name: {val.name}</h3>
+            <img src={val.img} />
+          </div>
+        );
+      })}
     </div>
   );
 }
